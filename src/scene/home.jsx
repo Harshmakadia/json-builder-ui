72cf0c8cb2c54adcd1b28ba5173afd189a40da16
Harsh <harsh.makadia@marutitech.com>
2019-03-04 15:42:04 +0530

    feat: goodbye bootstrap welcome semantic UI

--- src/scene/home.jsx
+++ src/scene/home.jsx
@@ -1,5 +1,6 @@
 // Importing Modules, Components and functions
 import React, { Component } from 'react';
+import { Container, Grid, Message, Divider } from 'semantic-ui-react';
 
 // Importing CSS files
 import './home.css';
@@ -18,9 +19,23 @@ class Home extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        Welcome to JSON Builder
-      </div>
+      <Container>
+        <Message
+            header='JSON Builder UI'
+            content='Start filing in the details below to get the JSON format of the details entered'
+        />
+        <Divider />
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <p>Enter the values here</p>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <p> Check the preview here</p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
